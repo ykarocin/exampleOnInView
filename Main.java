c4ce77cbc574fac4c43ff945b7e1683f7cc192b3
ykarocin <ysa@cin.ufpe.br>
2025-05-27 13:24:11 -0300

    Creating example with a main

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Detective detective = new Detective("Sherlock Holmes");
+
+        CaseFile case1 = new CaseFile("The Misterious Theft in the Museum");
+        case1.addClue(new Clue("Digital printing on the display window", 8));
+        case1.addClue(new Clue("Security camera failed", 6));
+        case1.addClue(new Clue("VIP report found", 9));
+
+        CaseFile case2 = new CaseFile("Disappearance in the Park");
+        case2.addClue(new Clue("Footprints found in the mud\r\n", 5));
+        case2.addClue(new Clue("Witness saw hooded figure", 6));
+
+        detective.addCaseFile(case1);
+        detective.addCaseFile(case2);
+
+        detective.solveCase(case1);
+        detective.solveCase(case2);
+    }
+}
